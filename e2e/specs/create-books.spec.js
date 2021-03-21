55db41a68ae5644e6e0144e579c829a47ef66644
Kliver Daniel <kliver1998@gmail.com>
2021-03-21 17:59:05 -0500

    Created test for save book

--- e2e/specs/create-books.spec.js
+++ e2e/specs/create-books.spec.js
@@ -1,15 +1,13 @@
 const axios = require('axios');
 const { expect } = require('chai');
-const { random } = require('faker');
+const faker = require('faker');
 const BASE_URL = 'https://books-back-qa.herokuapp.com';
-import {createUUID as UUID} from '../../util/functions';
 
 let response;
 
 const book = {
-    "id":UUID(),
-    "name":`Mrs. ${random.words(4)}`,
-    "author":``
+    "name":`Test: ${faker.random.words(4)}`,
+    "author":`Test: ${faker.name.findName()}`
 }
 describe("When user wants to create a book",() => {
     before(async()=>{
@@ -17,18 +15,16 @@ describe("When user wants to create a book",() => {
     });
 
     it('Then return a created status code',() => {
-        expect(response.status).eql(201);
+        expect(response.status).eql(200); //Back has not set to return 201, just default 200
     });
 
-    it('Then return the created animal',() => {
+    it('Then return the created book',() => {
         const createdBook = response.data;
-        expect(createdBook.id).eql(book.id);
         expect(createdBook.name).eql(book.name);
         expect(createdBook.author).eql(book.author);
     });
     
     it('Then return a json as content type',() => {
-        //console.log(response.headers)
         expect(response.headers['content-type']).to.contain('application/json');
     });
 });
