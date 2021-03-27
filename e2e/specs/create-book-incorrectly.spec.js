119ac4c5baee3d5d8d6a5c8d7acdd444636e4899
Kliver Daniel <kliver1998@gmail.com>
2021-03-26 21:27:39 -0500

    Coded negative tests

--- e2e/specs/create-book-incorrectly.spec.js
+++ e2e/specs/create-book-incorrectly.spec.js
@@ -1,29 +1,50 @@
 const axios = require('axios');
 const { expect } = require('chai');
-const faker = require('faker');
 const cs = require('../contants');
 
-let response;
+let book = {}
 
-axios.defaults.headers['Origin'] = 'https://books2testing-qa-not.herokuapp.com';
-const book = {
-    "name":`Test: ${faker.random.words(4)}`,
-    "author":`Test: ${faker.name.findName()}`
-}
+describe('When user wants to create a book providing not allowed values',() => {
+    describe('When name of book its an array',() => {
 
-describe("When user wants to create a book from not allowed origin",() => {
-    before(async()=>{
-        response = await axios.post(cs.baseUrl+'/books',book);
-        console.log('Hello')
+        it('Then should return a 400 Bad Request status code',async() => {
+            book = {
+                'name':[],
+                'author':'Test'
+            }
+            await axios.post(cs.baseUrl+'/books',book).catch(err => {
+                expect(err.response.status).to.equal(400)
+            });
+        });
+
     });
 
-    it.only('Then should return a 403 Forbidden status code',() => {
-        console.log(axios.defaults.headers)
-        expect(response.status).eql(403);
+    describe('When author of book its an array',() => {
+
+        it('Then should return a 400 Bad Request status code',async() => {
+            book = {
+                'name':'Test',
+                'author':[]
+            }
+            await axios.post(cs.baseUrl+'/books',book).catch(err => {
+                expect(err.response.status).to.equal(400)
+            });
+        });
+
     });
 
-    it('Then should ',() => {
-        console.log(response)
-    })
+    describe('When both name and author of book are arrays',() => {
+
+        it('Then should return a 400 Bad Request status code',async() => {
+            book = {
+                'name':[],
+                'author':[]
+            }
+            await axios.post(cs.baseUrl+'/books',book).catch(err => {
+                expect(err.response.status).to.equal(400)
+            });
+        });
+
+    });
     
 });
--- e2e/specs/delete-book-incorrectly.spec.js
+++ e2e/specs/delete-book-incorrectly.spec.js
@@ -1,13 +1,12 @@
 const axios = require('axios');
 const { expect } = require('chai');
-const faker = require('faker');
 const cs = require('../contants');
 
-describe('When user wants to update a book without providing id', () => {
+describe('When user wants to delete a book without providing id',() => {
 
     it('Then should return a 405 Method Not Allowed status code',async () => {
         let id = '';
-        await axios.put(`${cs.baseUrl}/books/${id}`).then(() => {
+        await axios.delete(`${cs.baseUrl}/books/${id}`).then(() => {
             expect(false).to.be.true; //If id it's empty and enter here, it's wrong
         }).catch(err => {
             expect(err.response.status).to.equal(405)
